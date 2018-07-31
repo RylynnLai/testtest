83bb30ebc2c6b4d4056cc0a7e28558b071da0d4f
longzhou <longzhou.lai@accenture.com>
2018-07-31 18:55:49 +0800

    init

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+protocol pppp {
+    var name : String {get set}
+    var age : Int {get}
+}
+
+struct sss: pppp {
+    var name: String
+    
+    var age: Int
+    
+    in
+}
